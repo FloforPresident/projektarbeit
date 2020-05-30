db2135f9e676c1e60f525ffca654ff8a348d50ea
FloforPresident <florian.geiger@kurzdigital.com>
2020-05-30 17:19:55 +0200

    Merge with testumgebung

---
+++ FlutterApp/turtlebot/lib/control.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Control extends StatelessWidget {
+  const Control({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Robo Control"),
+      ),
+    );
+  }
+}
