f01d72fb1efc49dd5b8574f9c2e9fc8abc62b4f1
Arbeit <schmittjo67900@th-nuernberg.de>
2020-09-22 15:00:14 +0200

    - Undone Database-Connection because of architecture change (Database now on ROS-Laptop)      preserved Files in archive
    - Used for top_app_bar.dart only this file and top_bar_image_icon.dart is now there aswell
    
    - Improved top_app_bar.dart

--- FlutterApp/turtlebot/lib/pages/pages_control/robo_status.dart
+++ FlutterApp/turtlebot/lib/pages/pages_control/robo_status.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:turtlebot/frameworks/custom_navigation_bar/top_app_bar.dart';
-import 'package:turtlebot/frameworks/custom_navigation_bar/top_bar_image_icon.dart';
 import 'package:turtlebot/services/routing.dart';
 import 'package:turtlebot/frameworks/custom_data_table/custom_dataTable.dart';
 
