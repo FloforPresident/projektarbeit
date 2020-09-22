34e3dce41742acb1a4ad30102ecdde7680985223
Arbeit <schmittjo67900@th-nuernberg.de>
2020-09-22 14:50:29 +0200

    Improved CustomDataTable is now only file and is already in a sizedbox so less complexity

--- FlutterApp/turtlebot/lib/frameworks/custom_data_table/custom_dataTable.dart
+++ FlutterApp/turtlebot/lib/frameworks/custom_data_table/custom_dataTable.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'custom_colour_text.dart';
 
-class CustomDataTable extends DataTable {
-  CustomDataTable({Key key,
+class CustomDataTable extends SizedBox {
+  CustomDataTable({
+    Key key,
     @required List<DataColumn> columns,
     int sortColumnIndex,
     bool sortAscending = true,
@@ -12,20 +12,26 @@ class CustomDataTable extends DataTable {
     double headingRowHeight = 56.0,
     double horizontalMargin = 0.0,
     double columnSpacing = 0.0,
-    @required List<DataRow> rows,})
-      : super(
-    columns: columns,
-    sortColumnIndex: sortColumnIndex,
-    sortAscending: sortAscending,
-    onSelectAll: onSelectAll,
-    dataRowHeight: kMinInteractiveDimension,
-    headingRowHeight: headingRowHeight,
-    rows: rows,
-    columnSpacing: 0.0,
-    horizontalMargin: 0.0,){}
-  static List<DataRow> generateStatusRows(Color backgroundColor, List<List<String>> dataTableRows) {
+    @required List<DataRow> rows,
+  }) : super(
+          width: double.infinity,
+          child: DataTable(
+              columns: columns,
+              sortColumnIndex: sortColumnIndex,
+              sortAscending: sortAscending,
+              onSelectAll: onSelectAll,
+              dataRowHeight: kMinInteractiveDimension,
+              headingRowHeight: headingRowHeight,
+              rows: rows,
+              columnSpacing: 0.0,
+              horizontalMargin: 0.0),
+        ) {}
 
-//  static List<DataRow> generateStatusRows(Color backgroundColor,TextStyle textStyle, List<List<String>> dataTableRows) {
+
+        //todo add possibility to add textstyles
+  static List<DataRow> generateStatusRows(
+      Color backgroundColor, List<List<String>> dataTableRows) {
+//  static List<DataRow> generateStatusRows(Color backgroundColor,TextStyle textStyle, List<List<String>> dataTableRows) todo {
 
     Iterator dataTableRowsIterator = dataTableRows.iterator;
     Iterator stringArrayIterator;
@@ -35,12 +41,12 @@ class CustomDataTable extends DataTable {
 
     Color currentColor;
 
-    for (int i = 0;dataTableRowsIterator.moveNext();i++) {
+    for (int i = 0; dataTableRowsIterator.moveNext(); i++) {
       currentColor = (i % 2 == 0) ? backgroundColor : Colors.white;
       stringArrayIterator = dataTableRows[i].iterator;
-      for(int y = 0; stringArrayIterator.moveNext() ;y++)
-      {
-        dataCells.add(DataCell(CustomColourText(Text(dataTableRows[i][y]),currentColor)));
+      for (int y = 0; stringArrayIterator.moveNext(); y++) {
+        dataCells.add(DataCell(
+            CustomColourText(Text(dataTableRows[i][y]), currentColor)));
 //        dataCells.add(DataCell(CustomColourText(Text(dataTableRows[i][y],style: textStyle),currentColor)));
 
       }
@@ -49,7 +55,49 @@ class CustomDataTable extends DataTable {
     }
 
     return dataRows;
-
   }
 
 }
+
+
+///Use this for better positioning of Column TextWidget
+class CustomColumnText extends StatelessWidget
+{
+
+  Text _textWidget;
+  EdgeInsets _padding;
+  CustomColumnText(this._textWidget, [this._padding = const EdgeInsets.all(15.0)]);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: _textWidget,
+      padding: _padding,
+    );
+  }
+
+}
+
+///Use this as instead of Datacells
+///Will produce a Container Widget taken up full space
+class CustomColourText extends StatelessWidget {
+
+  Color _backgroundColor;
+  final Text _text;
+  EdgeInsetsGeometry padding;
+
+  CustomColourText(this._text,[ this._backgroundColor = Colors.white,this.padding = const EdgeInsets.all(15.0)]);
+
+  Widget build(BuildContext context) {
+    return Container(child: _text,
+      color: _backgroundColor,
+      height: double.infinity,
+      width: double.infinity,
+      padding: EdgeInsets.all(15.0),);
+  }
+
+
+}
+
+
--- FlutterApp/turtlebot/lib/pages/pages_control/robo_status.dart
+++ FlutterApp/turtlebot/lib/pages/pages_control/robo_status.dart
@@ -3,23 +3,15 @@ import 'package:turtlebot/frameworks/custom_navigation_bar/top_app_bar.dart';
 import 'package:turtlebot/frameworks/custom_navigation_bar/top_bar_image_icon.dart';
 import 'package:turtlebot/services/routing.dart';
 import 'package:turtlebot/frameworks/custom_data_table/custom_dataTable.dart';
-import 'package:turtlebot/frameworks/custom_data_table/custom_colour_text.dart';
-import 'package:turtlebot/frameworks/custom_data_table/custom_column_text.dart';
-
 
 
 class RoboStatus extends StatefulWidget {
-
   final String _titleText = "RoboStatus";
-
 
   _RoboStatusState createState() => _RoboStatusState();
 }
 
-
-
-class _RoboStatusState extends State<RoboStatus>
-{
+class _RoboStatusState extends State<RoboStatus> {
   var ident = [
     ["Status", "Connected"],
     ["RoboName", "Robob"],
@@ -30,36 +22,33 @@ class _RoboStatusState extends State<RoboStatus>
     ["Room", "Living Room"],
   ];
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           leading: IconButton(
             icon: Icon(Icons.arrow_back),
-            onPressed: () { RouteGenerator.onTapToHome(context);},
+            onPressed: () {
+              RouteGenerator.onTapToHome(context);
+            },
           ),
-          title: TopAppBar(
-              [TopBarImageIcon(Icon(Icons.format_list_bulleted, color: Colors.white),RouteGenerator.onTapToRoboStatus),
-                TopBarImageIcon(Icon(Icons.computer),RouteGenerator.onTapToRoboCommands),
-                TopBarImageIcon(Icon(Icons.games),RouteGenerator.onTapToRoboManControl)],
-              widget._titleText
-          ),
+          title: TopAppBar([
+            TopBarImageIcon(
+                Icon(Icons.format_list_bulleted, color: Colors.white),
+                RouteGenerator.onTapToRoboStatus),
+            TopBarImageIcon(
+                Icon(Icons.computer), RouteGenerator.onTapToRoboCommands),
+            TopBarImageIcon(
+                Icon(Icons.games), RouteGenerator.onTapToRoboManControl)
+          ], widget._titleText),
           backgroundColor: Colors.purple,
-
         ),
-      body: SizedBox(
-        width: double.infinity,
-        child:       CustomDataTable(
-            columns: [DataColumn(label: CustomColumnText(Text(""))), DataColumn(label: CustomColumnText(Text(""))), ],
-            rows: CustomDataTable.generateStatusRows(Colors.blue, ident),
-
-      )
-
-
-      ),
-
-    );
+        body: CustomDataTable(
+          columns: [
+            DataColumn(label: CustomColumnText(Text(""))),
+            DataColumn(label: CustomColumnText(Text(""))),
+          ],
+          rows: CustomDataTable.generateStatusRows(Colors.blue, ident),
+        ));
   }
 }
