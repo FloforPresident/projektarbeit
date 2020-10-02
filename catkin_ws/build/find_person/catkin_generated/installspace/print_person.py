cd3b5cb154231d58578a81b4a44956b32caa7836
onemorebug <patrick.leupert@t-online.de>
2020-10-02 13:48:36 +0200

    Added several files

--- catkin_ws/build/find_person/catkin_generated/installspace/print_person.py
+++ catkin_ws/build/find_person/catkin_generated/installspace/print_person.py
@@ -39,18 +39,20 @@
 import rospy
 from std_msgs.msg import String
 
-def talker():
+#muss selber name sein wie rospy.init_node
+def print_person():
     pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('print_person', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+
     while not rospy.is_shutdown():
         hello_str = "Bocklet %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
+        rospy.loginfo("Published info: "+ hello_str + " sagt hallo patrick")
         pub.publish(hello_str)
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        talker()
+        print_person()
     except rospy.ROSInterruptException:
         pass
---
+++ catkin_ws/build/raspicam_node/catkin_generated/raspicam_node-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(raspicam_node_MESSAGE_FILES "/home/patrick/projektarbeit/catkin_ws/src/raspicam_node/msg/MotionVectors.msg")
+set(raspicam_node_SERVICE_FILES "")
