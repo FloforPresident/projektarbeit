9737cb7f8794ba70aa5ebb07b10000e5b9677657
bastl1212 <bastl121292@gmail.com>
2020-10-02 11:10:18 +0200

    test

--- catkin_ws/src/find_person/src/shout.cpp
+++ catkin_ws/src/find_person/src/shout.cpp
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "Max Mustermann" << count;
+    ss << "Max Mustermann test" << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
