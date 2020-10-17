3ad7b5030dd762187fe9426f1f809c76d917b56b
bastl1212 <bastl121292@gmail.com>
2020-10-17 12:31:10 +0200

    wat

--- catkin_ws/build/find_person/catkin_generated/installspace/datenbank_test.py
+++ catkin_ws/build/find_person/catkin_generated/installspace/datenbank_test.py
@@ -1,14 +1,27 @@
+#!/usr/bin/env python2
+import mysql.connector
+
 import rospy
 from std_msgs.msg import String
 
-#datenbank
-import mysql.connector
+def database():
+	pub = rospy.Publisher('chatter', String, queue_size=10)
+	rospy.init_node('print_person', anonymous=False)
 
-mydb = mysql.connector.connect(
-  host="localhost:8080/phpmyadmin/",
-  user="root",
-  password="",
-  database="test"
-)
+	mydb = mysql.connector.connect(
+	  host="localhost",
+	  user="root",
+	  password="",
+	  database="test"
+	)
 
-rospy.loginfo(print(mydb))
+	answer = mydb
+	rospy.loginfo(answer)
+	#pub.publish(answer)
+
+
+if __name__ == '__main__':
+    try:
+        database()
+    except rospy.ROSInterruptException:
+        pass
