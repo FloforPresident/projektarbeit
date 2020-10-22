81f851baf47781608b41917a7b39c35dde72e2c3
bastl1212 <bastl121292@gmail.com>
2020-10-22 14:18:58 +0200

    location monitor update

--- catkin_ws/build/location_monitor/catkin_generated/installspace/print_location.py
+++ catkin_ws/build/location_monitor/catkin_generated/installspace/print_location.py
@@ -7,23 +7,26 @@ from move_base_msgs.msg import MoveBaseActionFeedback
 
 
 def printLocation(data):
-	rate = rospy.Rate(10) # 10hz
+	#print("wird aufgerufen")
 
-    	while not rospy.is_shutdown():
-		#print("wird aufgerufen")
-	
-		print("x:")
-		print(data.feedback.base_position.pose.position.x)
-		print("y:")
-		print(data.feedback.base_position.pose.position.y)
+	print("x:")
+	print(data.feedback.base_position.pose.position.x)
+	print("y:")
+	print(data.feedback.base_position.pose.position.y)
 
-		rate.sleep()
 	
 def letsGo():
 	rospy.init_node('location_monitor', anonymous=True)
-	rospy.Subscriber("move_base/feedback", MoveBaseActionFeedback, printLocation)
-	#rospy.Subscriber("move_base_simple/goal", PoseStamped, printLocation)
-	rospy.spin()
+
+	#start topic
+	pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+	pub.publish();
+
+	rate = rospy.Rate(10) # 10hz
+	while not rospy.is_shutdown():
+		rospy.Subscriber("move_base/feedback", MoveBaseActionFeedback, printLocation)
+		rate.sleep()
+		rospy.spin()
 
 
 if __name__ == '__main__':
