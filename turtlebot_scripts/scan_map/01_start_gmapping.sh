f886c4b1b3e0b1baa897ca646947d8da6e602b27
bastl1212 <bastl121292@gmail.com>
2020-11-05 12:53:54 +0100

    skripte anpassen

--- turtlebot_scripts/scan_map/01_start_gmapping.sh
+++ turtlebot_scripts/scan_map/01_start_gmapping.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=gmapping
 
-
 #real robo
 #roslaunch turtlebot_navigation gmapping_demo.launch
 #roslaunch turtlebot_rviz_launchers view_navigation.launch
---
+++ turtlebot_scripts/scan_map/03_publish_key.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+publish key
+rostopic pub turtlebot3_teleop_keyboard/turtlebot3_teleop/turtlebot3_teleop_key 'w'
