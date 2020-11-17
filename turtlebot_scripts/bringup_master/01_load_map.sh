c738b2e07ca891ed246965e29fe153bc44a7196a
bastl1212 <bastl121292@gmail.com>
2020-11-17 08:53:59 +0100

    gitignore raus bei src turtlebot

--- turtlebot_scripts/bringup_master/01_load_map.sh
+++ turtlebot_scripts/bringup_master/01_load_map.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #load house map
 
-roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/patrick/stefan_zimmer.yaml' 
+#roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/patrick/stefan_zimmer.yaml' 
+roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/basti/projektarbeit/catkin_ws/maps/house.yaml' 
