ff671654f38f8a1723e25b2c1eedf09c809ed0b3
bastl1212 <bastl121292@gmail.com>
2020-12-22 08:31:13 +0100

    pfade angepasst

--- turtlebot_scripts/bringup_master/01_load_map.sh
+++ turtlebot_scripts/bringup_master/01_load_map.sh
@@ -2,4 +2,4 @@
 #load house map
 
 #roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/patrick/stefan_zimmer.yaml' 
-roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/basti/projektarbeit/backend/ros/catkin_ws/maps/house.yaml' 
+roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/basti/projektarbeit/backend/backend/catkin_ws/maps/house.yaml' 
