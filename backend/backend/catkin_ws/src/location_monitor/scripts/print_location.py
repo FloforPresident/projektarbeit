6f3310c83a5df828d2aad15c9fe82618b4c08a65
bastl1212 <bastl121292@gmail.com>
2020-12-22 09:20:48 +0100

    location monitor works

--- backend/backend/catkin_ws/src/location_monitor/scripts/print_location.py
+++ backend/backend/catkin_ws/src/location_monitor/scripts/print_location.py
@@ -8,43 +8,65 @@ from move_base_msgs.msg import MoveBaseActionFeedback, MoveBaseActionGoal, MoveB
 
 
 	
-#-----get current location constantaniously-----
+#----------
 def getLocation(locationData):
+	#current time
+	now = rospy.get_rostime()
 	currentX = locationData.feedback.base_position.pose.position.x
 	currentY = locationData.feedback.base_position.pose.position.y
 
 	print("x: " + str(currentX))
 	print("y: " + str(currentY))
 
+	#publish again to stay at current position
+	pubReset = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+	answerReset = PoseStamped()
+	answerReset.header.stamp = now
+	answerReset.header.frame_id = "map"
+	answerReset.pose.position.x = currentX
+	answerReset.pose.position.y = currentY
+	answerReset.pose.position.z = 0
+	answerReset.pose.orientation.w = 1.0
+
+	pubReset.publish(answerReset)
+
+
 
 #-------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     try:
+		#initialize node
 		rospy.init_node('location_monitor', anonymous=True)
+		#current time
+		now = rospy.get_rostime()
 		
-		#stop movement and twist
-		pub = rospy.Publisher('chatter', String, queue_size=10)
+		#publish once to move_base_simple/goal
+		pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+		answer = PoseStamped()
+		answer.header.stamp = now
+		answer.header.frame_id = "map"
+		answer.pose.position.x = 0
+		answer.pose.position.y = 0
+		answer.pose.position.z = 0
+		answer.pose.orientation.w = 1.0
+		#------------
 
-		datastring = '{"action": "stop", "name": "nobody"}'
-		dataArray = json.loads(datastring)
-
-		rospy.loginfo("Auf der Suche nach: " + dataArray["name"])
-		
-		rate1 = rospy.Rate(1) # 10hz
-		i = 0
-		while i < 4:
-			pub.publish(datastring)
-			i += 1
-			rate1.sleep()
-		
-		
 		#get current location
-		rate2 = rospy.Rate(10) # 10hz
-		while not rospy.is_shutdown():
+		rate2 = rospy.Rate(0.2) #0.5hz
+		rospy.Subscriber("move_base/feedback", MoveBaseActionFeedback, getLocation)
+		#wait until answer got published
+		rate2.sleep()
 
-			rospy.Subscriber("move_base/feedback", MoveBaseActionFeedback, getLocation)
-			rate2.sleep()
-			rospy.spin()
+		#alternativ
+		#stop movement and twist
+		#pub = rospy.Publisher('chatter', String, queue_size=10)
+		#datastring = '{"action": "stop", "name": "nobody"}'
+		#dataArray = json.loads(datastring)
+
+		#publish to move_base_simple to activate subscriber
+		pub.publish(answer)
+		
+		
 
     except rospy.ROSInterruptException:
         pass
