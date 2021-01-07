295f04d9bbeb33c996f83a713e32854dd722ad05
bastl1212 <bastl121292@gmail.com>
2021-01-07 14:48:02 +0100

    update turtlebot scripts

--- turtlebot_scripts/bringup_master/01_load_map.sh
+++ turtlebot_scripts/bringup_master/01_load_map.sh
@@ -2,4 +2,4 @@
 #load house map
 
 #roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/patrick/stefan_zimmer.yaml' 
-roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/basti/projektarbeit/basti_wohnung_1.yaml' 
+roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:='/home/basti/projektarbeit/basti_wohnung.yaml' 
